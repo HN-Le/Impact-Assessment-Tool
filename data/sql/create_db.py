8ecf243abb49c30fde69f44c6f7e5129e6c03eec
Tiny - Asus <hy.nhu.le@gmail.com>
2021-07-19 15:33:59 +0200

    added metric database

--- data/sql/create_db.py
+++ data/sql/create_db.py
@@ -2,6 +2,7 @@ import sqlite3
 from sqlite3 import Error
 import os
 import pandas as pd
+import numpy as np
 
 # from . import populate_core_data as core
 
@@ -10,6 +11,7 @@ def create_connection(db_file):
     conn = None
     try:
         conn = sqlite3.connect(db_file)
+        conn.execute("PRAGMA foreign_keys = 1")
         return conn
     except Error as e:
         print(e)
@@ -37,6 +39,25 @@ def main ():
     conn.close()
 
 def define_sql_table(conn):
+
+    sql_create_method_fragment_id = """ CREATE TABLE IF NOT EXISTS method_fragment (
+                                                 method_fragment_id integer PRIMARY KEY,
+                                                 method_fragment_name varchar(100) 
+                                             ); """
+
+    sql_create_metric_table = """CREATE TABLE IF NOT EXISTS metric (
+                                             metric_id integer PRIMARY KEY,
+                                             metric_name varchar(200) NOT NULL,
+                                             method_fragment_id integer,
+                                             metric_definition text,
+                                             metric_question text,
+                                             metric_value_type varchar(25),
+                                             multiple_answers bool,
+                                             answer_options text,
+                                             target_name varchar(50),
+                                             FOREIGN KEY (method_fragment_id) REFERENCES method_fragment (method_fragment_id)
+                                         );"""
+
     sql_create_project_table = """ CREATE TABLE IF NOT EXISTS project (
                                                  project_id integer PRIMARY KEY,
                                                  project_id_name varchar(200) 
@@ -44,7 +65,6 @@ def define_sql_table(conn):
 
     sql_create_metric_target_table = """ CREATE TABLE IF NOT EXISTS metric_target (
                                              metric_target_id integer PRIMARY KEY,
-                                             target_name varchar(50) NOT NULL,
                                              increase boolean NOT NULL,
                                              metric_target_value integer NOT NULL,
                                              project_id integer NOT NULL,
@@ -60,18 +80,6 @@ def define_sql_table(conn):
                                              FOREIGN KEY (project_id) REFERENCES project (project_id)
                                          );"""
 
-    sql_create_metric_table = """CREATE TABLE IF NOT EXISTS metric (
-                                             metric_id integer PRIMARY KEY,
-                                             metric_name varchar(200) NOT NULL,
-                                             method_fragment_id integer NOT NULL,
-                                             metric_definition text,
-                                             metric_question text,
-                                             metric_value_type varchar(25),
-                                             multiple_answers bool,
-                                             answer_options text,
-                                             FOREIGN KEY (method_fragment_id) REFERENCES method_fragment (method_fragment_id)
-                                         );"""
-
     sql_create_metric_value_table = """CREATE TABLE IF NOT EXISTS metric_value (
                                              metric_value_id integer PRIMARY KEY,
                                              measuring_point_id integer NOT NULL,
@@ -83,12 +91,15 @@ def define_sql_table(conn):
                                              FOREIGN KEY (measuring_point_id) REFERENCES measuring_point (measuring_point_id)
                                          );"""
 
-    sql_create_method_fragment_id = """ CREATE TABLE IF NOT EXISTS method_fragment (
-                                                 method_fragment_id integer PRIMARY KEY,
-                                                 method_fragment_name varchar(100) 
-                                             ); """
 
     if conn is not None:
+
+        # create task table
+        create_table(conn, sql_create_method_fragment_id)
+
+        # create task table
+        create_table(conn, sql_create_metric_table)
+
         # create project table
         create_table(conn, sql_create_project_table)
 
@@ -99,13 +110,8 @@ def define_sql_table(conn):
         create_table(conn, sql_create_measuring_point_table)
 
         # create task table
-        create_table(conn, sql_create_metric_table)
-
-        # create task table
         create_table(conn, sql_create_metric_value_table)
 
-        # create task table
-        create_table(conn, sql_create_method_fragment_id)
 
 
     else:
@@ -114,12 +120,25 @@ def define_sql_table(conn):
 
 def create_metric(conn, metric):
 
-    sql = """ INSERT INTO metric(metric_name, method_fragment_id, metric_definition, metric_question, metric_value_type) 
-            VALUES (?,?,?,?,?) """
-
     cur = conn.cursor()
+
+    sql = """ INSERT INTO metric(metric_name, method_fragment_id, metric_definition, metric_question, metric_value_type, multiple_answers, answer_options, target_name ) 
+            VALUES (?,?,?,?,?,?,?,?) """
+
     cur.execute(sql, metric)
     conn.commit()
+
+    # # check whether it is already present in database
+    # cur.execute("SELECT * FROM metric WHERE metric_name = ?", (metric[0],))
+    # entry = cur.fetchone()
+    #
+    # if entry is None:
+    #     cur.execute(sql, metric)
+    #     conn.commit()
+    #     print('New metric added')
+    #
+    # else:
+    #     print('Metric found')
 
     return cur.lastrowid
 
@@ -137,10 +156,10 @@ def create_method_fragment(conn, method_fragment):
     if entry is None:
         cur.execute(sql, method_fragment)
         conn.commit()
-        print('New entry added')
+        print('New method fragment added')
 
     else:
-        print('Entry found')
+        print('Method fragment found')
 
     return cur.lastrowid
 
@@ -152,9 +171,40 @@ def load_in_fragments(conn):
     dataframe = pd.DataFrame(raw_data)
 
     unique_values = dataframe.category.unique()
+    dataframe.fillna(np.nan, inplace=True)
 
     for row in unique_values:
         create_method_fragment(conn, ((row,)))
+
+
+    for index, row in dataframe.iterrows():
+        category = row['category']
+        metric_name = row['metric']
+        metric_definition = None
+        metric_question = row['survey_input']
+        metric_value_type = row['type']
+        answer_options = row['answer_options']
+        multiple_answers = row['multiple_answers_possible']
+        target_name = row['target']
+
+        if multiple_answers == 'yes':
+            multiple_answers = True
+
+        else:
+            multiple_answers = False
+
+        cur = conn.cursor()
+        cur.execute(""" SELECT method_fragment_id FROM method_fragment WHERE method_fragment_name = (?) """, (category,))
+        method_fragment_id = cur.fetchall()
+        method_fragment_id = int(method_fragment_id[0][0])
+
+        row = (metric_name, method_fragment_id, metric_definition,
+                    metric_question, metric_value_type, multiple_answers, answer_options, target_name)
+
+        create_metric(conn, row)
+
+
+
 
 if __name__ == '__main__':
     main()
